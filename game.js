a29f782058000b91c366986a921685be80751bca
HiJell0 <141372768+HiJell0@users.noreply.github.com>
2023-10-23 21:03:59 -0700

    Update and rename Vscode setup to game.js

---
+++ game.js
@@ -0,0 +1,37 @@
+body {
+  font-family: Arial, sans-serif;
+  background-color: #e6e6e6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+}
+
+#game-container {
+  background-color: white;
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.75);
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+#scene-image {
+  max-width: 100%;
+  height: auto;
+  border-radius: 5px;
+}
+
+#choice-buttons button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 5px;
+}
+
+#choice-buttons button:hover {
+  background-color: #45a049;
+}
