9d3e3ac3593e5bd73be6bb6fab56a2d3136b925e
HiJell0 <141372768+HiJell0@users.noreply.github.com>
2023-10-24 00:33:46 -0700

    Update game.js

--- game.js
+++ game.js
@@ -6,19 +6,19 @@ function makeChoice(choice) {
   switch (choice) {
     case 'choiceA':
       text.innerHTML = "You decided to head to the palace!";
-      image.src = "images/a1.jpg";
+      image.src = "images/a1.png";
       break;
 
     case 'choiceB':
       text.innerHTML = "You're heading to the dark island!";
-      image.src = "images/b1.jpg";
+      image.src = "images/b1.png";
       break;
 
     //... add more cases for additional choices ...
 
     default:
       text.innerHTML = "Your adventure begins in a mysterious forest. Which path will you take?";
-      image.src = "images/start.jpg";
+      image.src = "images/start.png";
   }
 
   // Additional game logic...
